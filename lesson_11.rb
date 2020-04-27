84b0f54bd915fc8adf75d02eba1b75362df5f40c
silvaauroram <61487992+silvaauroram@users.noreply.github.com>
2020-04-27 11:20:02 -0500

    Update lesson_11.rb

--- lesson_11.rb
+++ lesson_11.rb
@@ -40,3 +40,50 @@ number = gets.chomp.to_i
     puts ""
 
 end 
+
+
+
+# ------------------------
+#election time assignment
+
+puts "Welcome to my election voting program."
+puts ""
+puts "Election Candidates are: Lisa, Bart, Milhouse "
+
+
+countLisa = 0
+countBart = 0
+countMil = 0 
+
+10.times do |x|
+    puts "Vote ##{x+1}"
+    candidate = gets.chomp
+    candidate = candidate.upcase
+
+    if candidate == "LISA"
+        countLisa += 1 # += assigns vote
+    elsif candidate == "BART"
+        countBart += 1
+    elsif candidate == "MILHOUSE"
+        countMil += 1
+    else 
+        puts "Not a candidate"
+    end
+end 
+
+puts "--------ELECTION RESULTS--------"
+
+puts "Vote Summary:"
+puts "Lisa - #{countLisa} vote(s)"
+puts "Bart - #{countBart} vote(s)"
+puts "Milhouse - #{countMil} vote(s)"
+
+if countLisa > countBart && countLisa > countMil
+    puts "WINNER: Lisa!"
+elsif countBart > countLisa && countBart > countMil
+    puts "Winner: Bart!"
+elsif countMil > countLisa && countMil > countBart
+    puts "Winner: Milhouse"
+else 
+    puts "Its a tie"
+end
